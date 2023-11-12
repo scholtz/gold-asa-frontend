5a1c6784e019c5a488b54b1ee379616a6c90cf0e
Scholtz <ludko2@gmail.com>
2023-11-12 09:32:11 +0100

    testnet

---
+++ k8s/testnet/update-config.sh
@@ -0,0 +1,4 @@
+kubectl apply -f deployment-testnet.yaml -n asagold
+kubectl delete configmap asagold-testnet-conf -n asagold
+kubectl create configmap asagold-testnet-conf --from-file=conf -n asagold
+kubectl rollout restart deployment/asagold-web-testnet-deployment -n asagold
--- k8s/update-config.sh
+++
@@ -1,4 +0,0 @@
-kubectl apply -f deployment-main.yaml -n asagold
-kubectl delete configmap asagold-main-conf -n asagold
-kubectl create configmap asagold-main-conf --from-file=conf -n asagold
-kubectl rollout restart deployment/asagold-web-main-landing-deployment -n asagold
