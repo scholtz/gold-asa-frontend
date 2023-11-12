c541238178bd98324f4da14ce6247bef1179a931
Scholtz <ludko2@gmail.com>
2023-11-12 09:28:09 +0100

    cicd deploy

---
+++ k8s/mainnet/update-config.sh
@@ -0,0 +1,4 @@
+kubectl apply -f deployment-main.yaml -n asagold
+kubectl delete configmap asagold-main-conf -n asagold
+kubectl create configmap asagold-main-conf --from-file=conf -n asagold
+kubectl rollout restart deployment/asagold-web-main-deployment -n asagold
