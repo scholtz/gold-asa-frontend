cff5eef05daa5aff3952141a862e3d2936e6209b
Ludovit Scholtz <ludko2@gmail.com>
2023-12-04 17:51:27 +0100

    update home page spacing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,26 +5,26 @@ import ProductsCarousel from '@/components/ProductsCarousel.vue'
 
 <template>
   <Layout :hideTopMenu="false">
-    <div class="text-center sm:m-2 md:m-8">
+    <div class="text-center sm:m-4 md:m-8">
       <h1>Aurum Standard Asset</h1>
-      <div class="sm:m-1 md:m-4">
+      <div class="sm:m-2 md:m-4">
         <RouterLink to="/trade-gold">Buy</RouterLink>, use, or
         <RouterLink to="/trade-gold">sell</RouterLink> gold token backed by real gold
       </div>
-      <div class="sm:m-1 md:m-4">
+      <div class="sm:m-2 md:m-4">
         Everybody can <RouterLink to="/proof-of-reserve">audit our reserves</RouterLink> - its what
         we sell in the <RouterLink to="/buy-gold-coins">eshop</RouterLink>
       </div>
-      <div class="sm:m-1 md:m-4">
+      <div class="sm:m-2 md:m-4">
         Visit our <RouterLink to="/buy-gold-coins">gold eshop</RouterLink> including
         <RouterLink to="/buy-gold-coins">secondary marketplace</RouterLink>
       </div>
-      <div class="sm:m-1 md:m-4">
+      <div class="sm:m-2 md:m-4">
         Each gold coin has its
         <abbr title="Non Fungible Token - Each coin is unique">NFT</abbr>, which can be traded using
         smart contract or DeFi
       </div>
-      <div class="sm:m-1 md:m-4">
+      <div class="sm:m-2 md:m-4">
         NFTs in stock can be redeemed by parcel delivery within EU area
       </div>
     </div>
